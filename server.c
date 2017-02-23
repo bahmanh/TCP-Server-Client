aa3485e1eec5807a2f363fd394482af6be5995e0
bahmanh <bahmanhayat@gmail.com>
2017-02-23 04:21:03 -0800

    Socket setup

--- server.c
+++ server.c
@@ -9,8 +9,18 @@
 
 int main() {
     
-    return 0;
+    char buff[MAX_DATA];
+    struct sockaddr_in saddr_in;
+    int sock_fd, conn_fd;
 
+    bzero(&saddr_in, sizeof(saddr_in));
+    saddr_in.sin_family = AF_INET;
+    saddr_in.s_addr = INADDR_ANY;
+    saddr_in.sin_port = htons(PORT);
+
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+        fprintf(stderr, "Error with socket\n");
+    }
 
     
 }
