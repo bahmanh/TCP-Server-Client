aabe677631763781c94f49b6bd00aea1de0bec23
bahmanh <bahmanhayat@gmail.com>
2017-02-23 04:20:22 -0800

    Setting up

---
+++ server.c
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+
+#define MAX_DATA = 256
+#define PORT = 3228
+
+int main() {
+    
+    return 0;
+
+
+    
+}
